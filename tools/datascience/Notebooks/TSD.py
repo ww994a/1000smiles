803cc6e0c0813f33cad26ab0109d383478a956da
ww994a <116334124+ww994a@users.noreply.github.com>
2023-11-16 11:46:36 -0800

    Updated TSD.py
    Now includes Category Data for the patient in df_merged.

--- tools/datascience/Notebooks/TSD.py
+++ tools/datascience/Notebooks/TSD.py
@@ -25,7 +25,14 @@ from os.path import isfile, join
 
 #centralized location for file paths
 
-file_prefix = '../data/'
+
+import sys
+import os
+
+# Get the current directory
+current_directory = os.path.abspath(os.path.dirname(__file__))
+file_prefix = current_directory+'/../data/'
+
 
 file_loc_clinic = file_prefix+'clinic_clinic-final.txt'
 file_loc_register = file_prefix+'register_register-final.txt'
@@ -79,7 +86,7 @@ df_merged = pd.merge(df_patient, df_merged, left_on="id", right_on="patient_id")
 
 
 #Colonia not named
-df_merged.fillna({"colonia":"unlisted"},inplace=True)
+df_merged.fillna({"colonia":"Unlisted"},inplace=True)
                 
 
 #clinic dataframes
@@ -103,5 +110,42 @@ df_routing['clinic_id'] = df_routing['clinic_id'].map(df_clinic['start'])
 
 #medical history
 df_medical = pd.read_csv(file_loc_history, encoding="latin-1", sep="__", engine="python")
+
+med_srs = df_medical[["cold_cough_fever", "hivaids", "anemia", "athsma", "cancer", "congenitalheartdefect", "diabetes", "epilepsy", "bleeding_problems", "hepititis", "tuberculosis", "troublespeaking", "troublehearing", "troubleeating", "born_with_cleft_lip", "born_with_cleft_palate","patient_id"]].copy()
+
+column_ = ["Cold Cough Fever", "HIV/AIDS", "Anemia", "Athsma",
+            "Cancer", "Congenital Heart Defect", "Diabetes", "Epilepsy",
+            "Bleeding Problems", "Hepititis", "Tuberculosis", "Trouble Speaking",
+            "Trouble Hearing", "Troubleeating", "Born With Cleft Lip", 
+             "Born With Cleft Palate","patient_id"]
+med_srs = med_srs.set_axis(column_, axis=1)
+
+med_srs['Category'] = med_srs.apply(lambda row: [col for col in med_srs.iloc[:,:-1].columns if row[col] == 1], axis = 1)
+
+
+#erase duplicate records choosing the one with a longer list of conditions
+
+med_srs['Values_length'] = med_srs['Category'].apply(len)
+
+med_srs = med_srs.sort_values(by='Values_length', ascending=False).drop('Values_length', axis=1)
+
+# Drop duplicates, keeping only the row with the longest 'Values' array
+med_srs = med_srs.drop_duplicates(subset='patient_id', keep='first')
+
+# Reset the index if needed
+med_srs = med_srs.reset_index(drop=True)
+
+def replace_empty_list(lst):
+    return ['Unlisted'] if not lst else lst
+
+med_srs['Category'] = med_srs['Category'].apply(replace_empty_list)
+
+df_medical['Category'] = med_srs['Category']
+
+
+short_med = med_srs[['patient_id','Category']]
+df_merged = pd.merge(df_merged, short_med, on='patient_id', how='left')
+
+
 
 
